6ab49025ed8b791b3ec147d5d11d05971be0acbd
Rami Waked <rwaked1@hotmail.com>
2020-07-29 21:19:15 -0500

    Add files via upload

---
+++ build/precache-manifest.bc36dfb3f1a7e9279cabd6dd658ea107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3746b12373c9030e783513efca20e469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "079650cfb891406a24dc",
+    "url": "/static/css/main.424bcbf5.chunk.css"
+  },
+  {
+    "revision": "3c41a260ed8d321172be",
+    "url": "/static/js/2.d6976f74.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.d6976f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079650cfb891406a24dc",
+    "url": "/static/js/main.baeb7760.chunk.js"
+  },
+  {
+    "revision": "83b7cc9f86aaf60b17f5",
+    "url": "/static/js/runtime-main.9076f898.js"
+  }
+]);
